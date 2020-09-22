5198c43afa8c12baed03d1532c3b14d0618e8eaa
kundan keshidi <kundankeshidik@gmail.com>
2020-09-21 18:40:12 -0700

    added autoconfiguration and made changes in resources file to application properties for logging debug

--- springboot-in-10steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10stepsApplication.java
+++ springboot-in-10steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10stepsApplication.java
@@ -1,13 +1,20 @@
 package com.in28minutes.springboot.basics.springbootin10steps;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10stepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10stepsApplication.class, args);
+		ApplicationContext applicationContext =
+				SpringApplication.run(SpringbootIn10stepsApplication.class, args);
+		for(String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
+		
 	}
 
 }
